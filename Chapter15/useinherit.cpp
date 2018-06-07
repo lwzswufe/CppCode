e22b6e9c150dba42b5e2c2e1ac1b20195b42c8b6
lwzswufe <lwzswufe@foxmail.com>
2018-06-07 23:00:05 +0800

    Chapter15 数据成员指针 成员函数指针

--- Chapter15/useinherit.cpp
+++ Chapter15/useinherit.cpp
@@ -22,5 +22,22 @@ int main()
     cout << base_b.sum(1, 2, 3) << endl;    // B 共有的继承了base 这里调用的是Base版本的sum
     cout << base_c.sum(1, 2) << endl;       // C 私有的继承了base 但是在public部分using Base::std 这里调用的是Base版本的sum
     cout << base_c.sum(1, 2, 3) << endl;
+
+    Base* bptr_0 = &base_0;
+    Base* bptr_b = &base_b;
+
+    using PMfi = int(Base::*)(int, int);    // 成员函数指针 指向类的相对位置
+    PMfi pf = &Base::sum;                   // 初始化指针
+    cout << (base_0.*pf)(2, 3) << endl;     // 调用基类函数
+    cout << (base_a.*pf)(2, 3) << endl;     // 调用派生类重构后的函数
+    cout << (bptr_0->*pf)(2, 3) << endl;    // 通过类指针调用成员函数指针
+    cout << (bptr_b->*pf)(2, 3) << endl; 
+
+    using PM = const string Base::*;        // 数据成员指针 指向类的相对位置
+    PM pm = &Base::name;
+    cout << base_0.*pm << endl;
+    cout << base_a.*pm << endl;
+    cout << bptr_0->*pm << endl;            // 通过类指针调用数据成员
+    cout << bptr_b->*pm << endl; 
     return 0;
 }
