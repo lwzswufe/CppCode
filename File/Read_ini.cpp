ab94057f01a67ec58cbbb371baff0516141d17b3
lwzswufe <lwzswufe@foxmail.com>
2018-07-13 13:33:49 +0800

    读取配置文件

--- File/Read_ini.cpp
+++ File/Read_ini.cpp
@@ -1,11 +1,8 @@
-#include <iostream>
-using std::cout; using std::endl;
-
 #include "Read_ini.h"
 
 int main()
 {   
-    char fn[100] = "TradeX-L2.ini";
+    char fn[100] = ".\\data\\TradeX-L2.ini";
     char s1[256] = "[COMMON]\n";
     char s2[256] = "\n";
     char s3[256] = "BakIPADDRESS = 210.51.55.211";
@@ -13,4 +10,7 @@ int main()
     line_parse(s1, key, value);
     line_parse(s2, key, value);
     line_parse(s3, key, value);
+    
+    auto config = Read_ini(fn);
+    show_ini(config);
 }
--- File/Read_ini.h
+++ File/Read_ini.h
@@ -1,21 +1,42 @@
 #include <map>
 #include <fstream>
 #include <cstring>
-#include <iostream>
-using std::cerr; using std::cout; using std::endl; using std::ends;
+
+
+void line_parse(char line[256], std::string &key, std::string &value)
+{   // 拆分一行字符串  
+    char * pch_1;
+    char * pch_2;
+    char * next_token = NULL;
+    pch_1   = strtok_s(line, "= []\n", &next_token);
+    if (pch_1 != NULL)
+        key = pch_1;
+    else
+        key = "";
+
+    pch_2 = strtok_s(NULL, "= []\n", &next_token);
+    if (pch_2 != NULL)
+        value = pch_2;
+    else
+        value = "";
+}
+
 
 std::map<std::string, std::map<std::string, std::string>> 
 Read_ini(char filename[100])
-{   
-    char line[256];
+{   // 读取配置文件
+    size_t line_size{256};
+    char line[line_size];
     std::string key, value, temp_key{""};
-    std::map<std::string, std::string> temp_map;
+
     std::map<std::string, std::map<std::string, std::string>> config;
     std::ifstream input;
+
     input.open(filename);
+
     while(!input.eof())
     {
-        getline(input, line);
+        input.getline(line, line_size);
         line_parse(line, key, value);
         if (key == "")
         {
@@ -23,41 +44,33 @@ Read_ini(char filename[100])
         }
         if (value == "")
         {   
-            if (temp_key == "")
-            {   
-                config[temp_key] = temp_map;
-                temp_map.clear();
-            }
+            std::map<std::string, std::string> temp_map;
+            config[key] = temp_map;
             temp_key = key;
         }
         else
         {
-            temp_map[key] = value;
+            config[temp_key][key] = value;
         }
-    }
-    if (temp_key == "")
-    {   
-        config[temp_key] = temp_map;
-        temp_map.clear();
     }
     return config;
 }
 
-void line_parse(char line[256], std::string &key, std::string &value)
-{
-    char * pch_1;
-    char * pch_2;
-    pch_1   = strtok(line, "= []\n");
-    if (pch_1 != NULL)
-        key = pch_1;
-    else
-        key = "";
-
-    pch_2 = strtok(NULL, "= []\n");
-    if (pch_2 != NULL)
-        value = pch_2;
-    else
-        value = "";
-
-    //cout << key << ": " << value << endl;
+void show_ini(std::map<std::string, std::map<std::string, std::string>> config)
+{   //显示配置文件的内容
+    char buffer[100];
+    for (auto i= config.begin(); i!= config.end(); i++)
+    {   
+        std::string key = i->first;
+        auto value      = i->second;
+        sprintf_s(buffer, "[%s]:\n", key.data());
+        fputs (buffer , stdout);  
+        for (auto ii= value.begin(); ii != value.end(); ii++)
+        {
+            std::string key_   = ii->first;
+            std::string value_ = ii->second;
+            sprintf_s(buffer, "        <%s : %s>\n", key_.data(), value_.data());
+            fputs (buffer , stdout);  
+        }
+    }
 }
