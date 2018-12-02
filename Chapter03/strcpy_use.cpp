53865ddc8e48a115a3a947e1bf9655c3e59a9b0a
lwzswufe <lwzswufe@foxmail.com>
2018-12-02 22:42:18 +0800

    更新strcpy_s的使用

--- Chapter03/strcpy_use.cpp
+++ Chapter03/strcpy_use.cpp
@@ -1,5 +1,6 @@
 #include <cstdio>
 #include <cstring>
+typedef char *Str;
 // 复制字符数组
 int main()
 {
@@ -8,6 +9,7 @@ int main()
     char str2[40];
     char str3[40];
     char str4[40];
+    Str str5 = new char[40];
     const char strc[20] = "const char []";
     const char *str_p = &strc[0];
     strcpy (str2,str1);
@@ -20,5 +22,8 @@ int main()
 
     strcpy_s(str4,str_p);
     printf_s("strp: %s\nstr4: %s\n\n", str_p, str4);
+    
+    strcpy_s(str5, 40, str_p);        // 不可直接使用    strcpy_s(str5, str_p) 40用于指明str5的大小
+    printf_s("strp: %s\nstr4: %s\n\n", str_p, str5);
     return 0;
 }
