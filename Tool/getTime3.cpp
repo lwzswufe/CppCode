dca88d88332c2bf83289c684ae9ff0ea75c73063
lwzswufe <lwzswufe@foxmail.com>
2019-01-22 23:15:14 +0800

    多线程测试 获取毫秒时间字符串

--- Tool/getTime3.cpp
+++ Tool/getTime3.cpp
@@ -4,17 +4,30 @@
 #include<stdio.h> 
 
 using namespace std;
+
 double getCurrentTime()
 {   
-    double time_d;
     struct timeval tv;    
-    mingw_gettimeofday(&tv,NULL);    
+    mingw_gettimeofday(&tv, NULL);    
     return tv.tv_sec % 3600 + tv.tv_usec / 1000.0;  
+}
+
+string getCurrentTimeStr()
+{   
+    struct timeval tv;    
+    mingw_gettimeofday(&tv, NULL);   
+    char s[64];
+    long seconds = tv.tv_sec % 86400;
+    //cout << seconds << ", " << tv.tv_usec << endl;
+    sprintf_s(s, "%02d:%02d:%02d.%03d", 8 + seconds / 3600, seconds / 60 % 60, seconds % 60, tv.tv_usec / 1000);
+    return s;  
 }
 
 int main()
 {
 	double d = getCurrentTime();
+    string s = getCurrentTimeStr();
 	cout << "seconds" << d << endl;
+    cout << s << endl;
 	return 0;
 }
