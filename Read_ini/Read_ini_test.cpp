e407f381635b141b6d7654b8a7b78d6f7011531c
lwzswufe <lwzswufe@foxmail.com>
2019-04-18 23:52:00 +0800

    通过makefile编译调试linux多文件C++项目

--- Read_ini/Read_ini_test.cpp
+++ Read_ini/Read_ini_test.cpp
@@ -1,12 +1,22 @@
 #include "Read_ini.h"
 #include <vector>
+#include <iostream>
 using std::vector;
 /*
 g++ -std=c++11 Read_ini_test.cpp Read_ini.h Read_ini.cpp -o Read_ini_test && "/home/wiz/CppCode/File/"Read_ini_test
 */
 int main()
 {   
-    IniType conf = Read_ini("Trade.ini");
+    IniType conf;
+    try
+    {
+        conf = Read_ini("Trade.ini");
+    }
+    catch(const std::exception& err)
+    {
+        std::cerr << err.what() << std::endl;
+        return -1;
+    }
     show_ini(conf);
     printf("Read End\n");
     return 0;
