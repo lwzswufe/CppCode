33bed2a4c684b01396bf17d9509db92a20e281e8
lwzswufe <lwzswufe@foxmail.com>
2020-03-18 23:04:40 +0800

    使用std::hex输出nan

--- Chapter01/nan.cpp
+++ Chapter01/nan.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <math.h>
-
+#include <iostream>
 int main()
 {   
-    double d = nan("Not A Number");
+    double d = nan("NAN");
     printf("d = %lf  d * 1 = %lf d * 0 = %lf d + 1 = %lf\n", d, d * 1, d * 0, d + 1);
+    std::cout << "d: " << std::hex << d << std::endl; 
+    printf("d = %x\n", d);
     return 0;
 }
